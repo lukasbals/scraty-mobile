43dfb432c89cbf078d034634f39ccaf0822fef86
Lukas Bals <mail@lukasbals.at>
2020-04-02 10:56:24 +0200

    Initialized expo project

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./components/HomeScreen";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Scraty" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,17 @@
+# Scraty mobile
+
+This is the [react-native](https://reactnative.dev) application for the open source
+project [scraty](https://github.com/mikethebeer/scraty). The project is based on
+[expo](https://expo.io).
+
+## Available commands
+
+```bash
+yarn install # To install dependencies
+
+yarn start # To start the local development server
+
+yarn run [ios|android|web] # To run the app on specific platform
+
+yarn run lint # To lint the project
+```
