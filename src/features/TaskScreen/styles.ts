85ad46430c3c5984bfdf6bdb90b613bf2a4c9ca3
Lukas Z <lzi6116@students.fhv.at>
2020-04-21 11:54:21 +0200

    added Task and Taskview
    added Taskview to render Tasks;
    includes feature to show different names in different colors;
    random lint fail?;

--- src/features/TaskScreen/styles.ts
+++ src/features/TaskScreen/styles.ts
@@ -4,4 +4,31 @@ export default StyleSheet.create({
     scene: {
         flex: 1,
     },
+    area: {
+        borderWidth: 1,
+        borderColor: 'black',
+        margin: 5,
+    },
+    rowView: {
+        marginHorizontal: 5,
+        flexDirection: 'row',
+        justifyContent: "space-between",
+    },
+    columnView: {
+        marginVertical: 5,
+        flexDirection: 'column',
+        justifyContent: "space-evenly",
+    },
+    taskTitle: {
+        fontSize: 24,
+    },
+    taskDesc: {
+        fontSize: 16,
+    },
+    taskPerson: {
+        fontSize: 20,
+    },
+    button: {
+        fontSize: 30,
+    },
 });
